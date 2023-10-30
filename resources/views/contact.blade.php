d4683e8f46fccd821fcfb2163d9eb899f866f5c4
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-10-30 15:28:06 +0800

    修改先前各子樣板中沒有修改到的 Page Header 內容

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,9 +8,9 @@
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
-                        <div class="site-heading">
-                            <h1>Clean Blog</h1>
-                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                        <div class="page-heading">
+                            <h1>Contact Me</h1>
+                            <span class="subheading">Have questions? I have answers.</span>
                         </div>
                     </div>
                 </div>
